be7733d451842c3b648a0205c5e7d33011ac193d
Размик Нерсесян <razo12321@gmail.com>
2025-03-28 17:28:26 +0300

    0.0.1

--- DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -11,20 +11,7 @@ from app import forms, views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
-    path('login/',
-         LoginView.as_view
-         (
-             template_name='app/login.html',
-             authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
-         ),
-         name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('admin/', admin.site.urls),
+    path('login/',views.login_view, name='login'),
+    path("logout/", views.login_view, name="logout"),
+
 ]
--- DjangoWebProject1/app/views.py
+++ DjangoWebProject1/app/views.py
@@ -4,43 +4,54 @@ Definition of views.
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+from email import message
 from django.shortcuts import render
 from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.templatetags.static import static
 
+@login_required
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
+    username = request.user.username  # Получаем логин пользователя
+    default_avatar = static('app/image/NoAvatar.jpg')
     return render(
         request,
-        'app/login.html',
+        'app/index.html',
         {
-            'title':'Login',
-            'year':datetime.now().year,
+            'message': '',
+            "username": username,
+            "avatar": "",
+            'default_avatar': default_avatar,
         }
     )
 
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
 
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+def logout_view(request):
+    logout(request)# Выход из аккаунта
+    return redirect("login_view")  # Перенаправление на страницу входа
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")  # Получаем логин
+        password = request.POST.get("password")  # Получаем пароль
+
+        user = authenticate(request, username=username, password=password)  # Проверяем пользователя
+
+        if user is not None:  # Если логин/пароль верные
+            login(request, user)
+            return render(request, 'app/login.html', {"error": "Верный логин и пароль"})  # Ошибка  # Перенаправляем на главную страницу
+        else:
+            return render(request, 'app/login.html', {"error": "Неверный логин или пароль"})  # Ошибка
+
+    return render(request, 'app/login.html')  # Если `GET`, просто показываем форму
+
+
+
