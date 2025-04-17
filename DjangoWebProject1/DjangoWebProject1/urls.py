b03b9faa9867dc14d01e25a6fe3ff3357d11bfe8
Размик Нерсесян <razo12321@gmail.com>
2025-04-17 11:59:24 +0300

    Реализовано отображение собщений по заявке

--- DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/',views.login_view, name='login_view'),
     path("logout/", views.logout_view, name="logout"),
-    path("Open_Ticket/", views.Open_Ticket, name="Open_Ticket"),
-
-
+    path("Open_Ticket/<str:ticket_uuid>", views.Open_Ticket, name="Open_Ticket"),
+    path("filter-incidents/", views.filter_incidents, name="filter_incidents"),
 ]
--- DjangoWebProject1/app/views.py
+++ DjangoWebProject1/app/views.py
@@ -15,48 +15,67 @@ from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.templatetags.static import static
+from requests.auth import HTTPBasicAuth
 import requests
 import json
 import pyodbc
-
-
-from requests.auth import HTTPBasicAuth
-
+import html
+import re
+from bs4 import BeautifulSoup
+# === Блок динамического обновления страниц === ↓
 
 @login_required
-def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    username = request.user.username  # Получаем логин пользователя
+def filter_incidents(request): # Динамическое обновение списка обращений пользователя
+    state = request.GET.get("state", "all")
+    all_incidents = list(get_incidents_list(request.user.initiator_uuid))
 
-    default_avatar = static('app/image/NoAvatar.jpg')
-    a = get_incidents_list(request.user.initiator_uuid)
+    if state == "Всего":
+        filtered = all_incidents
+    elif state == "Открыто":
+        filtered = [i for i in all_incidents if i.get("state") != 'Закрыто']
+    else:
+        filtered = [i for i in all_incidents if i.get("state") == 'Закрыто']
+
+    return render(request, "app/partials/incidents_list.html", {"incidents": filtered})
 
 
+def clean_html(text):
+    decoded = html.unescape(text)
+    soup = BeautifulSoup(decoded, "html.parser")
 
+    def is_visually_empty(tag):
+        if tag.find(["img", "iframe"]):
+            return False
+        clean_text = re.sub(r'[\s\u00a0]+', '', tag.get_text())
+        return not clean_text
 
+    # Удаляем только реально пустые
+    for tag in soup.find_all(["p", "span", "div"]):
+        if is_visually_empty(tag):
+            tag.decompose()
 
+    return str(soup)
 
-
-
+@login_required
+def Open_Ticket(request, ticket_uuid):
+    all_incidents = extGetDetailIncidentInfo(ticket_uuid)
+    # Раскодируем HTML в каждом сообщении
+    for msg in all_incidents.get("TheHistoryOfCommunication", []):
+        msg["html_render"] = clean_html(msg.get("HTMLText", ""))
     return render(
-        request,
-        'app/index.html',
+        request, 
+        'app/Open_Ticket.html',
         {
             'message': '',
-            "username": username,
-            "avatar": "",
-            'default_avatar': default_avatar,
-            'incidents': a,
+            "username": request.user.username,
+            'incident_info': all_incidents,
+
         }
-        
+) # Страница подробной информации о тикете
 
-    )
+# === Блок Взаимодействия с API и обработка данныз === ↓
 
-
-
-
-def get_incidents_list(response):
+def get_incidents_list(response): # Получение списка обращений пользователя 
     url = "http://m9-intalev-1c/ITIL/hs/externalapi/getIncidentsList"
     
     # JSON-данные, которые ожидает API
@@ -84,7 +103,7 @@ def get_incidents_list(response):
     else:
         return {"error": f"Ошибка {response.status_code}", "details": response.text}
 
-def get_initiator_uuid(data):
+def get_initiator_uuid(data): # Получение UUID пользователя из getInitiators1 
     try:
         # Преобразуем строку JSON в словарь
         parsed_data = json.loads(data)
@@ -97,9 +116,9 @@ def get_initiator_uuid(data):
         else:
             return None  # Если в "Initiators" нет данных
     except json.JSONDecodeError:
-        return None  # Если ошибка в формате JSON
+        return None 
 
-def getInitiators1(request):
+def getInitiators1(request): # Получение информации о пользователе через его логин 
     url = "http://m9-intalev-1c/ITIL/hs/externalapi/getInitiators1"
     
     # JSON-данные, которые ожидает API
@@ -123,19 +142,67 @@ def getInitiators1(request):
     else:
         return {"error": f"Ошибка {response.status_code}", "details": response.text}
 
+def extGetDetailIncidentInfo(request): # Получение информации о пользователе через его логин 
+    url = "http://m9-intalev-1c/ITIL/hs/externalapi/extGetDetailIncidentInfo"
+    
+    # JSON-данные, которые ожидает API
+    payload = {
+        "Uuid": request
+        }
 
+    # Учетные данные (замени на свои)
+    username = "r.nersesyan"
+    password = "1234"
 
+    response = requests.post(
+        url,
+        json=payload,
+        auth=HTTPBasicAuth(username, password),
+        headers={"Content-Type": "application/json"}
+    )
+
+    if response.status_code == 200:
+        f = json.loads(response.text)
+        array = f
+        return array
+    else:
+        return {"error": f"Ошибка {response.status_code}", "details": response.text}
+
+# === Блок открытия страниц === ↓
 
 @login_required
-def Open_Ticket(request):
-    return render(request, 'app/Open_Ticket.html')
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    username = request.user.username  # Получаем логин пользователя
 
+    default_avatar = static('app/image/NoAvatar.jpg')
+    incidents =list(get_incidents_list(request.user.initiator_uuid))
+    count_closed = sum(1 for incident in incidents if incident.get("state") == 'Закрыто')
+    count_open = sum(1 for incident in incidents if incident.get("state") != 'Закрыто')
+    count_all = len(incidents)
+
+    return render(
+        request,
+        'app/index.html',
+        {
+            'message': '',
+            "username": username,
+            "avatar": "",
+            'default_avatar': default_avatar,
+            'incidents': incidents,
+            'count_closed': count_closed,
+            'count_open': count_open,
+            'count_all': count_all,
+        }
+        
+
+    ) # Страница всех тикетов пользователя
 
 @login_required
 def logout_view(request):
     logout(request)# Выход из аккаунта
-    return redirect("login_view")  # Перенаправление на страницу входа
-
+    return redirect("login_view")  # Выход из профиля
 
 def login_view(request):
 
@@ -163,6 +230,3 @@ def login_view(request):
             return render(request, 'app/login.html', {"error": "Неверный логин или пароль"})  # Ошибка
 
     return render(request, 'app/login.html')  # Если `GET`, просто показываем форму
-
-
-
